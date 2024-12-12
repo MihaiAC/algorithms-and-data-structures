7f53127e2a6fcf6b2baa40f10e74c940b4e5e2e7
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-12-13 01:44:05 +0200

    Solve p2 from aoc2024 day 12 by counting the corners instead of the sides.

--- advent_of_code_2024/day12/p2.py
+++ advent_of_code_2024/day12/p2.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import Tuple
 
 class Solution:
     def __init__(self, input_file: str):
@@ -7,61 +8,72 @@ class Solution:
             for line in f:
                 if line != '\n':
                     self.matrix.append(line[:-1])
+        self.M, self.N = len(self.matrix), len(self.matrix[0])
+
+        self.DELTAS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        self.WINDOWS = [
+            [(-1, 0), (0, 1), (-1, 1)],
+            [(0, 1), (1, 0), (1, 1)],
+            [(1, 0), (0, -1), (1, -1)],
+            [(-1, 0), (0, -1), (-1, -1)]
+        ]
     
+    def within_bounds(self, coord_x: int, coord_y: int) -> bool:
+        return 0 <= coord_x < self.M and 0 <= coord_y < self.N
+
+    def count_corners(self, coords: Tuple[int, int]) -> int:
+        cx, cy = coords
+        corner_count = 0
+        current_letter = self.matrix[cx][cy]
+        for window in self.WINDOWS:
+            same_letter_count = 0
+            out_of_bounds_count = 0
+            for dx, dy in window[:2]:
+                if not self.within_bounds(cx+dx, cy+dy):
+                    out_of_bounds_count += 1
+                elif self.matrix[cx+dx][cy+dy] == current_letter:
+                    same_letter_count += 1
+            
+            if same_letter_count == 2:
+                dx, dy = window[2]
+                if self.matrix[cx+dx][cy+dy] != current_letter:
+                    corner_count += 1
+            elif same_letter_count == 0 and out_of_bounds_count == 0:
+                corner_count += 1
+            elif same_letter_count == 0 and out_of_bounds_count == 1:
+                corner_count += 1
+            elif out_of_bounds_count == 2:
+                corner_count += 1
+
+        return corner_count    
+
     def calculate_price(self) -> int:
         total_price = 0
-        M, N = len(self.matrix), len(self.matrix[0])
-        visited = [[0 for _ in range(N)] for _ in range(M)]
-        deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        orthogonal = {
-            (0, 1): [(-1, 0), (1, 0)],
-            (0, -1): [(-1, 0), (1, 0)],
-            (1, 0): [(0, 1), (0, -1)],
-            (-1, 0): [(0, 1), (0, -1)]
-        }
-        within_bounds = lambda x, y: 0 <= x < M and 0 <= y < N
+        visited = [[0 for _ in range(self.N)] for _ in range(self.M)]
 
-        for ii in range(M):
-            for jj in range(N):
+        for ii in range(self.M):
+            for jj in range(self.N):
                 if visited[ii][jj] == 0:
+                    visited[ii][jj] = 1
+                    
                     current_letter = self.matrix[ii][jj]
                     current_sides = 0
                     current_area = 0
-                    queue = deque()
-                    queue.appendleft((ii, jj))
+                    
+                    queue = deque([(ii, jj)])
+
                     while len(queue) > 0:
                         cx, cy = queue.pop()
-                        if visited[cx][cy]:
-                            continue
-                        else:
-                            visited[cx][cy] += 1
                         current_area += 1
-                        for dx, dy in deltas:
-                            if within_bounds(cx+dx, cy+dy):
-                                if self.matrix[cx+dx][cy+dy] != current_letter:
-                                    counted_side = False
-                                    for ox, oy in orthogonal[(dx, dy)]:
-                                        if within_bounds(cx+ox, cy+oy):
-                                            if visited[cx+ox][cy+oy] and self.matrix[cx+ox][cy+oy] == current_letter and self.matrix[cx+ox+dx][cy+oy+dy] != current_letter:
-                                                counted_side = True
-                                                break
-                                    if not counted_side:
-                                        current_sides += 1
-                                elif visited[cx+dx][cy+dy] == 0:
-                                    queue.appendleft((cx+dx, cy+dy))
-                            else:
-                                counted_side = False
-                                for ox, oy in orthogonal[(dx, dy)]:
-                                    if within_bounds(cx+ox, cy+oy):
-                                        if visited[cx+ox][cy+oy] and self.matrix[cx+ox][cy+oy] == current_letter and not within_bounds(cx+ox+dx, cy+oy+dy):
-                                            counted_side = True
-                                            break
-                                if not counted_side:
-                                    current_sides += 1
+                        current_sides += self.count_corners((cx, cy))
+                        for dx, dy in self.DELTAS:
+                            if self.within_bounds(cx+dx, cy+dy) and self.matrix[cx+dx][cy+dy] == current_letter and visited[cx+dx][cy+dy] == 0:
+                                visited[cx+dx][cy+dy] = 1
+                                queue.appendleft((cx+dx, cy+dy))
                     total_price += current_area * current_sides        
         return total_price
 
 
 if __name__ == '__main__':
-    sol = Solution('test')
+    sol = Solution('input')
     print(sol.calculate_price())
