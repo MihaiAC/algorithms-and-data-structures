a82b6c3e3d6aac904fdd0d5f8f3e4f2d03a047df
MihaiAC <alexmihai.ciobanu@gmail.com>
2024-12-23 17:45:15 +0200

    Add day 23 p2 using networkx.

--- advent_of_code_2024/day23/p2.py
+++ advent_of_code_2024/day23/p2.py
@@ -2,62 +2,21 @@ from collections import defaultdict
 from typing import List
 from copy import deepcopy
 
+import networkx as nx
+
 class Solution:
     def __init__(self, input_file: str):
         lines = open(input_file).read().strip().split('\n')
         
-        self.neighbors = defaultdict(set)
+        self.graph = nx.Graph()
         for line in lines:
             u, v = line.split('-')
-            self.neighbors[u].add(v)
-            self.neighbors[v].add(u)
-        
-        self.nodes = list(self.neighbors.keys())
-        self.available_nodes = set(self.neighbors.keys())        
-        self.longest_clique = []
-        self.visited = set()
-    
-    def add_clique_to_visited(self, current_clique: List[str]):
-        arr = deepcopy(current_clique)
-        arr.sort()
-        self.visited.add(tuple(arr))
-    
-    def clique_in_visited(self, current_clique: List[str]) -> bool:
-        arr = deepcopy(current_clique)
-        arr.sort()
-        return tuple(arr) in self.visited
-
-    def find_all_cliques(self, current_clique: List[str]):
-        if len(current_clique) > len(self.longest_clique):
-            self.longest_clique = deepcopy(current_clique)
-        
-        if self.clique_in_visited(current_clique):
-            return
-        
-        print(current_clique)
-        for node in self.nodes:
-            if node in self.available_nodes:
-                connected = True
-                # Check that node has a connection to all of the nodes in the current_clique.
-                for clique_node in current_clique:
-                    if node not in self.neighbors[clique_node]:
-                        connected = False
-                        break
-                
-                if connected:
-                    current_clique.append(node)
-                    self.available_nodes.remove(node)
-                    
-                    self.find_all_cliques(current_clique)
-                    
-                    current_clique.pop()
-                    self.available_nodes.add(node)
-        
+            self.graph.add_edge(u, v) 
 
     def find_max_len_clique(self) -> str:
-        self.find_all_cliques([])
-        self.longest_clique.sort()
-        return ",".join(self.longest_clique)
+        longest_clique = list(nx.approximation.max_clique(self.graph))
+        longest_clique.sort()
+        return ",".join(longest_clique)
 
         
 if __name__ == '__main__':
