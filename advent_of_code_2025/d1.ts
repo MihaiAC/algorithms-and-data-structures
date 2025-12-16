4b504db5692dce119b66a358f057897fd0e2e896
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-16 16:20:02 +0000

    [TS] Add AOC2025 day 1 part 2.

--- advent_of_code_2025/d1.ts
+++ advent_of_code_2025/d1.ts
@@ -12,6 +12,23 @@ function normalize(x: number): number {
     return x % 100;
 }
 
+/**
+ * Brings x into the 0-99 range, counting the number of
+ * times it crosses 0.
+ * @param x
+ * @returns [normalized_x, count]
+ */
+function normalizeAndCount(x: number): [number, number] {
+    let count = 0;
+    while (x < 0) {
+        x += 100;
+        count += 1;
+    }
+
+    count += Math.floor(x / 100);
+    return [x % 100, count];
+}
+
 function p1(rotations: string[]): number {
     let count = 0;
     let curr = 50;
@@ -34,11 +51,35 @@ function p1(rotations: string[]): number {
     return count;
 }
 
+function p2(rotations: string[]): number {
+    let count = 0;
+    let curr = 50;
+
+    for (const rotation of rotations) {
+        const direction = rotation[0];
+        const magnitude = Number(rotation.slice(1));
+        let next, add;
+
+        if (direction === "L") {
+            [next, add] = normalizeAndCount(curr - magnitude);
+        } else {
+            [next, add] = normalizeAndCount(curr + magnitude);
+        }
+
+        count += add!;
+        curr = next!;
+    }
+
+    return count;
+}
+
 const getRotations = (fileName: string) =>
     fs.readFileSync(fileName, "utf-8").trim().split("\n");
 
 const exampleRotations = getRotations("d1-p1-example.txt");
 assert.equal(p1(exampleRotations), 3);
+assert.equal(p2(exampleRotations), 6);
 
 const p1Rotations = getRotations("d1-p1.txt");
 console.log(p1(p1Rotations));
+console.log(p2(p1Rotations));
