59e7285cf15e595cedab664009ddddcd56e69853
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-19 16:19:28 +0000

    [AOC2025, TS] Add day2 part 2.

--- advent_of_code_2025/d2.ts
+++ advent_of_code_2025/d2.ts
@@ -1,7 +1,36 @@
 import assert from "node:assert";
 import * as fs from "fs";
 
-function p1(ranges: number[][]): number {
+function isInvalidP1(numStr: string): boolean {
+    if (numStr.length % 2 === 0) {
+        const halfIdx = Math.floor(numStr.length / 2);
+        if (numStr.slice(0, halfIdx).endsWith(numStr.slice(halfIdx))) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isInvalidP2(numStr: string): boolean {
+    const N = numStr.length;
+    // Would be faster to only analyse divisors.
+    for (let chunkSize = 1; chunkSize <= Math.floor(N / 2); chunkSize++) {
+        if (N % chunkSize === 0) {
+            const base = numStr.slice(0, chunkSize);
+            if (numStr === base.repeat(Math.floor(N / chunkSize))) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function calculateSum(
+    ranges: number[][],
+    isInvalid: (numStr: string) => boolean
+): number {
     let sum = 0;
     for (const range of ranges) {
         assert(range.length === 2, `Incorrect range in input: ${range}`);
@@ -10,12 +39,8 @@ function p1(ranges: number[][]): number {
         const max = range[1]!;
 
         for (let num = min; num <= max; num++) {
-            const numStr = String(num);
-            if (numStr.length % 2 === 0) {
-                const halfIdx = Math.floor(numStr.length / 2);
-                if (numStr.slice(0, halfIdx).endsWith(numStr.slice(halfIdx))) {
-                    sum += num;
-                }
+            if (isInvalid(String(num))) {
+                sum += num;
             }
         }
     }
@@ -30,5 +55,7 @@ const getRanges = (fileName: string) =>
         .split(",")
         .map((x) => x.split("-").map((y) => Number(y)));
 
-console.log(p1(getRanges("d2-example.txt")));
-console.log(p1(getRanges("d2-p1.txt")));
+console.log(calculateSum(getRanges("d2-example.txt"), isInvalidP1));
+console.log(calculateSum(getRanges("d2-p1.txt"), isInvalidP1));
+console.log(calculateSum(getRanges("d2-example.txt"), isInvalidP2));
+console.log(calculateSum(getRanges("d2-p1.txt"), isInvalidP2));
