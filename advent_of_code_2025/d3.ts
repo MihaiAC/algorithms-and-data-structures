ced645229358fbb550b6bab761d9d88efc9ad0eb
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-20 20:03:30 +0000

    [AOC2025, TS] Add day 3 part 2.

--- advent_of_code_2025/d3.ts
+++ advent_of_code_2025/d3.ts
@@ -1,3 +1,4 @@
+import assert from "assert";
 import * as fs from "fs";
 
 function p1(joltages: string[]): number {
@@ -20,7 +21,45 @@ function p1(joltages: string[]): number {
     return maxSum;
 }
 
+function p2(joltages: string[], nDials: number): number {
+    let maxSum = 0;
+    const stringLen = joltages[0]!.length;
+
+    assert(nDials <= stringLen, "TOO MANY DIALS, MAN!");
+
+    for (const joltage of joltages) {
+        const digits = [];
+        for (let idx = stringLen - nDials; idx < stringLen; idx++) {
+            digits.push(joltage.at(idx)!);
+        }
+
+        for (let idx = stringLen - nDials - 1; idx >= 0; idx--) {
+            const digit = joltage.at(idx)!;
+            if (digit >= digits[0]!) {
+                let carry = digits[0]!;
+                digits[0]! = digit;
+                for (let digitIdx = 1; digitIdx < nDials; digitIdx++) {
+                    if (carry >= digits[digitIdx]!) {
+                        const tmp = digits[digitIdx]!;
+                        digits[digitIdx]! = carry;
+                        carry = tmp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        maxSum += Number(digits.join(""));
+    }
+
+    return maxSum;
+}
+
 const getJoltages = (fileName: string) =>
     fs.readFileSync(fileName, "utf-8").split(/\r?\n/);
 console.log(p1(getJoltages("d3-example.txt")));
 console.log(p1(getJoltages("d3-input.txt")));
+console.log(p2(getJoltages("d3-example.txt"), 2));
+console.log(p2(getJoltages("d3-example.txt"), 12));
+console.log(p2(getJoltages("d3-input.txt"), 12));
