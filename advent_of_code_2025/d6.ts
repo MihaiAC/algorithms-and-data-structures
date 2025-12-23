2a11474155c08402fa801c1175813d60fea33bb8
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-23 16:08:59 +0200

    [AOC2025, TS] Add day 6 part 2.

--- advent_of_code_2025/d6.ts
+++ advent_of_code_2025/d6.ts
@@ -36,8 +36,52 @@ function p1(numbers: number[][], signs: string[]): number {
     return ans;
 }
 
+function p2(fileName: string): number {
+    const lines = fs.readFileSync(fileName, "utf-8").split(/\r?\n/);
+    const signs = lines.pop()!;
+    const nrows = lines.length;
+
+    const parseCol = (colIdx: number) => {
+        const currNum: string[] = [];
+        for (let rowIdx = nrows - 1; rowIdx >= 0; rowIdx--) {
+            if (lines[rowIdx][colIdx] !== " ") {
+                currNum.push(lines[rowIdx][colIdx]);
+            }
+        }
+
+        // Column is a separator.
+        if (currNum.length === 0) {
+            return undefined;
+        }
+
+        return Number(currNum.reverse().join(""));
+    };
+
+    let ans = 0;
+    let partial = 0;
+    let currSign: "+" | "*" = "+";
+    for (let colIdx = 0; colIdx < signs.length; colIdx++) {
+        if (signs[colIdx] === "+" || signs[colIdx] === "*") {
+            currSign = signs[colIdx]! as "+" | "*";
+            partial = ACCUM_START[currSign];
+        }
+
+        const currNum = parseCol(colIdx);
+        if (currNum === undefined) {
+            ans += partial;
+        } else {
+            partial = OPS[currSign](partial, currNum);
+        }
+    }
+
+    return ans + partial;
+}
+
 const [exampleNumbers, exampleSigns] = readInput("d6-example.txt");
 console.log(p1(exampleNumbers, exampleSigns));
 
 const [inputNumbers, inputSigns] = readInput("d6-input.txt");
 console.log(p1(inputNumbers, inputSigns));
+
+console.log(p2("d6-example.txt"));
+console.log(p2("d6-input.txt"));
