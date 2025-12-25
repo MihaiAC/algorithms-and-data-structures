9e7c76271525b8dd54f79d7afae0a917e6714bd8
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-25 19:26:55 +0200

    [AOC2025, TS] Try day 10 part 2 with GLPK.
    Cannot make GLPK work :(

--- advent_of_code_2025/d10.ts
+++ advent_of_code_2025/d10.ts
@@ -1,6 +1,9 @@
-// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 import * as fs from "fs";
 import assert from "node:assert";
+import Worker from "web-worker";
+// Polyfill Worker for Node.js
+global.Worker = Worker as any;
+import GLPK from "glpk.js";
 
 type Machine = {
     target: string;
@@ -89,7 +92,53 @@ function p1(machines: Machine[]): number {
     return machines.reduce((accum: number, curr: Machine) => accum + minOps(curr), 0);
 }
 
+async function minButtonPresses(machine: Machine): Promise<number> {
+    const glpk = await GLPK();
+    const N = machine.buttons.length;
+
+    const problem = {
+        name: "ButtonPresses",
+        objective: {
+            direction: glpk.GLP_MIN,
+            name: "presses",
+            vars: machine.buttons.map((_, idx) => ({
+                name: `x${idx}`,
+                coef: 1.0,
+            })),
+        },
+        subjectTo: machine.joltReqs.map((req, counterIdx) => ({
+            name: `counter${counterIdx}`,
+            vars: machine.buttons
+                .map((button, btnIdx) =>
+                    button.includes(counterIdx) ? { name: `x${btnIdx}`, coef: 1.0 } : null
+                )
+                .filter((v) => v !== null),
+            bnds: { type: glpk.GLP_FX, ub: req, lb: req },
+        })),
+        bounds: machine.buttons.map((_, idx) => ({
+            name: `x${idx}`,
+            type: glpk.GLP_LO,
+            lb: 0.0,
+            ub: 500.0,
+        })),
+    };
+
+    const result = glpk.solve(problem);
+    return Math.round((await result).result.z);
+}
+
+async function p2(machines: Machine[]): Promise<number> {
+    let total = 0;
+    for (const machine of machines) {
+        total += await minButtonPresses(machine);
+    }
+
+    return total;
+}
+
 const exampleMachines = readInput("d10-example.txt");
 const inputMachines = readInput("d10-input.txt");
 console.log(p1(exampleMachines));
 console.log(p1(inputMachines));
+
+console.log(await p2(exampleMachines));
