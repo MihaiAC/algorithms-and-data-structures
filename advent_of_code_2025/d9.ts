405eb8de0442611bf4bf8af3417261e5223e7cc9
Mihai <alexmihai.ciobanu@gmail.com>
2025-12-25 12:58:33 +0200

    [AOC2025, TS] Clean up day 9 part 1.

--- advent_of_code_2025/d9.ts
+++ advent_of_code_2025/d9.ts
@@ -1,17 +1,24 @@
 import * as fs from "fs";
 
-function p1(coords: number[][]): number {
-    // Quick and dirty, want to see p2 (sensing something nasty ~~~)
-    const N = coords.length;
-    const getArea = (p1: number[], p2: number[]): number => {
-        return Math.abs(p1[0]! - p2[0]! + 1) * Math.abs(p1[1]! - p2[1]! + 1);
+type Point = { x: number; y: number };
+
+/**
+ * Quick and dirty, want to see p2 (sensing something nasty ~~~)
+ * @param points
+ * @returns
+ */
+function p1(points: Point[]): number {
+    const N = points.length;
+
+    const getArea = (p1: Point, p2: Point): number => {
+        return Math.abs(p1.x - p2.x + 1) * Math.abs(p1.y - p2.y + 1);
     };
 
     let ans = 0;
     for (let idx1 = 0; idx1 < N - 1; idx1++) {
-        const p1 = coords[idx1]!;
+        const p1 = points[idx1]!;
         for (let idx2 = idx1 + 1; idx2 < N; idx2++) {
-            const p2 = coords[idx2]!;
+            const p2 = points[idx2]!;
             ans = Math.max(ans, getArea(p1, p2));
         }
     }
@@ -19,12 +26,23 @@ function p1(coords: number[][]): number {
     return ans;
 }
 
-const readInput = (fileName: string): number[][] => {
+const readInput = (fileName: string): Point[] => {
     return fs
         .readFileSync(fileName, "utf8")
         .split(/\r+\n/)
-        .map((line: string) => line.split(",").map((x) => Number(x)));
+        .map((line: string) => {
+            const [x, y] = line.split(",").map(Number);
+            return { x, y };
+        });
 };
+
+/**
+ * Surprise! It was pretty nasty.
+ * @param coords
+ */
+function p2(coords: number[][]): number {
+    return 0;
+}
 
 const example = readInput("d9-example.txt");
 const input = readInput("d9-input.txt");
