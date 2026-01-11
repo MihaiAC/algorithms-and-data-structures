0145762a01b1c99e370fa4c448d71968be9311df
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-11 12:58:06 +0000

    [TS] Add daily LC - optimal.

--- practice/LC_TypeScript/maximal-rectangle.ts
+++ practice/LC_TypeScript/maximal-rectangle.ts
@@ -2,30 +2,33 @@ import assert from "node:assert";
 
 function maximalRectangle(matrix: string[][]): number {
     const [M, N] = [matrix.length, matrix[0]!.length];
-
-    const maxWidth: number[][] = Array.from({ length: M }, () => Array(N).fill(0));
-    for (let ii = 0; ii < M; ii++) {
-        maxWidth[ii][N - 1] = Number(matrix[ii][N - 1]);
-        for (let jj = N - 2; jj >= 0; jj--) {
-            if (matrix[ii][jj] === "0") maxWidth[ii][jj] = 0;
-            else maxWidth[ii][jj] = maxWidth[ii][jj + 1] + 1;
-        }
-    }
-
+    const widths: number[] = Array(M + 1).fill(0);
     let maxArea = 0;
-    for (let ii = 0; ii < M; ii++) {
-        for (let jj = 0; jj < N; jj++) {
-            if (matrix[ii][jj] === "1") {
-                let currArea = maxWidth[ii][jj];
-                maxArea = Math.max(maxArea, currArea);
 
-                let currWidth = maxWidth[ii][jj];
-                let kk = ii + 1;
-                while (kk < M && matrix[kk][jj] === "1") {
-                    currWidth = Math.min(currWidth, maxWidth[kk][jj]);
-                    maxArea = Math.max(maxArea, currWidth * (kk - ii + 1));
-                    kk += 1;
+    for (let jj = N - 1; jj >= 0; jj--) {
+        const stack: number[] = [-1];
+
+        for (let ii = 0; ii <= M; ii++) {
+            if (ii < M) {
+                if (matrix[ii][jj] === "1") {
+                    widths[ii] += 1;
+                } else {
+                    widths[ii] = 0;
                 }
+            }
+
+            while (
+                stack.length > 1 &&
+                (ii === M || widths[ii]! < widths[stack[stack.length - 1]!]!)
+            ) {
+                const prevRow = stack.pop()!;
+                const height = ii - stack[stack.length - 1]! - 1;
+                const area = widths[prevRow]! * height;
+                maxArea = Math.max(maxArea, area);
+            }
+
+            if (ii < M) {
+                stack.push(ii);
             }
         }
     }
