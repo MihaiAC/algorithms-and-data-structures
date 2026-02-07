53a5883f4c21ba7010808b7674227fa800e4bcfe
Mihai <alexmihai.ciobanu@gmail.com>
2026-02-07 10:18:36 +0000

    [TS] Further improve daily LC.
    Remove extra variable.

--- practice/LC_TypeScript/minimum-deletions.ts
+++ practice/LC_TypeScript/minimum-deletions.ts
@@ -1,17 +1,15 @@
 import assert from "node:assert";
 
 function minimumDeletions(s: string): number {
-    let numA = 0;
     let minDeletions = 0;
     let currB = 0;
 
     for (let currIdx = 0; currIdx < s.length; currIdx++) {
         if (s[currIdx] === "b") currB += 1;
-        else numA += 1;
         minDeletions = Math.min(minDeletions, 2 * currB - currIdx - 1);
     }
 
-    return minDeletions + numA;
+    return minDeletions + s.length - currB;
 }
 
 assert.equal(minimumDeletions("aababbab"), 2);
