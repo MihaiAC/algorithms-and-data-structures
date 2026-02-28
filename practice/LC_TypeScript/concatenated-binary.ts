27209d0490693a5ddd36c0c5c145778d1bdc8fa8
Mihai <alexmihai.ciobanu@gmail.com>
2026-02-28 10:53:30 +0000

    [TS] Build left to right instead, remove extra bits.

--- practice/LC_TypeScript/concatenated-binary.ts
+++ practice/LC_TypeScript/concatenated-binary.ts
@@ -1,28 +1,19 @@
-import { memoize } from "lodash";
 import assert from "node:assert";
 
 const MODN = 1000000007n;
 
-const expModMemo = memoize(
-    (exp: bigint, mod: bigint): bigint => {
-        if (exp == 0n) return 1n;
-        if (exp % 2n === 0n) {
-            const half = expModMemo(exp / 2n, mod);
-            return (half * half) % mod;
-        } else {
-            return (2n * expModMemo(exp - 1n, mod)) % mod;
-        }
-    },
-    (exp, mod) => `${exp},${mod}`
-);
-
 function concatenatedBinary(n: number): number {
     let ans = 0n;
-    let bins = 0n;
-    for (let num = n; num >= 1; num--) {
-        const nDigits = BigInt(num.toString(2).length);
-        ans = (ans + BigInt(num) * expModMemo(bins, MODN)) % MODN;
-        bins += nDigits;
+    let len = 0n;
+    let nextPow2 = 1;
+
+    for (let num = 1; num <= n; num++) {
+        if (num === nextPow2) {
+            len += 1n;
+            nextPow2 *= 2;
+        }
+
+        ans = ((ans << len) + BigInt(num)) % MODN;
     }
 
     return Number(ans);
