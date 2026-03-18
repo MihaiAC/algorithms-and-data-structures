c4a6dc97cd23e7ed99c638c48740edc39c2380f1
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-03-18 14:48:25 +0200

    [TS] String matching LCQ, P1 - use single regexp.

--- LC_TypeScript/quests/string-matching/repeated-substring-pattern.ts
+++ LC_TypeScript/quests/string-matching/repeated-substring-pattern.ts
@@ -1,15 +1,7 @@
 import assert from "node:assert";
 
 function repeatedSubstringPattern(s: string): boolean {
-    if (s.length === 1) return false;
-
-    // For all divisors of s.length, check if s = s[0:div]+.
-    for (let divisor = 1; divisor <= Math.floor(s.length / 2); divisor++) {
-        const pattern = new RegExp(`^(${s.slice(0, divisor)})+$`, "g");
-        if (pattern.test(s)) return true;
-    }
-
-    return false;
+    return /^(\w+)\1{1,}$/.test(s);
 }
 
 assert.equal(repeatedSubstringPattern("abab"), true);
