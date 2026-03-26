e8c47d4d5acf75910d0ff5319a7162992f76d8d7
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-03-26 19:05:35 +0200

    [TS] Add ListNode class + some convenience methods.

---
+++ LC_TypeScript/ListNode.ts
@@ -0,0 +1,41 @@
+export class ListNode {
+    val: number;
+    next: ListNode | null;
+
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+
+    /**
+     * Builds a ListNode from an array of numbers.
+     */
+    public static fromArray(arr: number[]): ListNode | null {
+        if (arr.length === 0) return null;
+
+        const head = new ListNode(arr[0]);
+        let curr = head;
+        for (const num of arr.slice(1)) {
+            let newNode = new ListNode(num);
+            curr.next = newNode;
+            curr = newNode;
+        }
+
+        return head;
+    }
+
+    /**
+     * Check linked list equality.
+     */
+    public equals(node: ListNode | null): boolean {
+        if (node === null) return false;
+        let head: ListNode | null = this;
+
+        while (head && node && head.val === node.val) {
+            head = head.next;
+            node = node.next;
+        }
+
+        return head === null && node === null;
+    }
+}
