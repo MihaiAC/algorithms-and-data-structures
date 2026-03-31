95ee2f4546874826e2ec4e87bb5d187bc8dd6071
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-03-31 19:34:25 +0300

    [TS] Add LCQ prefix sum P2 - OOM.

---
+++ LC_TypeScript/quests/prefix-sum/make-sum-div-by-p.ts
@@ -0,0 +1,35 @@
+import assert from "node:assert";
+
+function minSubarray(nums: number[], p: number): number {
+    const N = nums.length;
+
+    let sum = 0;
+    for (let idx = 0; idx < N; idx++) {
+        nums[idx] = nums[idx] % p;
+        sum += nums[idx];
+    }
+
+    sum = sum % p;
+    if (sum === 0) return 0;
+
+    const modLastIdx = new Array(p).fill(-1);
+    let currMod = 0;
+    let ans = N;
+    for (let idx = 0; idx < N; idx++) {
+        currMod = (currMod + nums[idx]) % p;
+        if (currMod === sum) ans = Math.min(ans, idx + 1);
+
+        const searchMod = (currMod - sum + p) % p;
+        if (modLastIdx[searchMod] !== -1)
+            ans = Math.min(ans, idx - modLastIdx[searchMod]);
+
+        modLastIdx[currMod] = idx;
+    }
+
+    return ans < N ? ans : -1;
+}
+
+assert.equal(minSubarray([3, 1, 4, 2], 6), 1);
+assert.equal(minSubarray([6, 3, 5, 2], 9), 2);
+assert.equal(minSubarray([1, 2, 3], 3), 0);
+assert.equal(minSubarray([5], 7), -1);
