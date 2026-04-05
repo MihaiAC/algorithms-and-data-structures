77a6f539d1b0490604da08220db74a71aad6ec17
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-05 11:31:54 +0300

    [TS] Optimise daily LC (18ms -> 9ms).

--- LC_TypeScript/easy/657.ts
+++ LC_TypeScript/easy/657.ts
@@ -1,22 +1,16 @@
 import assert from "node:assert";
 
-const map: Record<string, number[]> = {
-    U: [0, 1],
-    D: [0, -1],
-    L: [1, 0],
-    R: [-1, 0],
-};
-
 function judgeCircle(moves: string): boolean {
-    const leftUp = [0, 0];
+    let [dx, dy] = [0, 0];
 
     for (const move of moves) {
-        const [dx, dy] = map[move]!;
-        leftUp[0] += dx;
-        leftUp[1] += dy;
+        if (move === "U") dy++;
+        else if (move === "D") dy--;
+        else if (move === "L") dx--;
+        else dx++;
     }
 
-    return leftUp[0] === 0 && leftUp[1] === 0;
+    return dx === 0 && dy === 0;
 }
 
 assert.equal(judgeCircle("UD"), true);
