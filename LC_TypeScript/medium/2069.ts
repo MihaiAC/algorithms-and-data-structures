98ff15fdf7b46f1e29760a191a84f73ec09c02c0
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-07 12:31:09 +0300

    [TS] Add daily LC - TLE.
    If on edge, need to steps % perimeter.

---
+++ LC_TypeScript/medium/2069.ts
@@ -0,0 +1,84 @@
+import assert from "node:assert";
+
+type Direction = "North" | "South" | "West" | "East";
+
+const delta: Record<Direction, number[]> = {
+    North: [0, 1],
+    South: [0, -1],
+    West: [-1, 0],
+    East: [1, 0],
+};
+
+const kaiten: Record<Direction, Direction> = {
+    North: "West",
+    West: "South",
+    South: "East",
+    East: "North",
+};
+
+class Robot {
+    M: number;
+    N: number;
+    cDir: Direction = "East";
+    x: number = 0;
+    y: number = 0;
+
+    constructor(width: number, height: number) {
+        this.M = width;
+        this.N = height;
+    }
+
+    withinBounds(x: number, y: number): boolean {
+        return 0 <= x && x < this.M && 0 <= y && y < this.N;
+    }
+
+    clampX(x: number): number {
+        return Math.min(this.M - 1, Math.max(0, x));
+    }
+
+    clampY(y: number): number {
+        return Math.min(this.N - 1, Math.max(0, y));
+    }
+
+    step(steps: number): void {
+        const [dx, dy] = delta[this.cDir];
+        let [nx, ny] = [this.x + dx * steps, this.y + dy * steps];
+
+        if (this.withinBounds(nx, ny)) {
+            [this.x, this.y] = [nx, ny];
+            return;
+        }
+
+        // Clamp nx, ny to be within bounds.
+        [nx, ny] = [this.clampX(nx), this.clampY(ny)];
+
+        // Subtract the number of steps taken to reach the edge.
+        steps -= Math.abs(this.x - nx) - Math.abs(this.y - ny);
+
+        // Update robot position and direction.
+        [this.x, this.y] = [nx, ny];
+        this.cDir = kaiten[this.cDir];
+
+        // Call step recursively.
+        this.step(steps);
+    }
+
+    getPos(): number[] {
+        return [this.x, this.y];
+    }
+
+    getDir(): string {
+        return this.cDir;
+    }
+}
+
+const robot = new Robot(6, 3);
+robot.step(2);
+robot.step(2);
+assert.deepEqual(robot.getPos(), [4, 0]);
+assert.deepEqual(robot.getDir(), "East");
+robot.step(2);
+robot.step(1);
+robot.step(4);
+assert.deepEqual(robot.getPos(), [1, 2]);
+assert.equal(robot.getDir(), "West");
