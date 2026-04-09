130e5a8d1b9df1e91fd5a8ccb555d05a2d062c83
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-09 21:45:21 +0300

    [Python] Generate graph visualisation, add missing deps.

--- advent_of_code/2024/day23/p2.py
+++ advent_of_code/2024/day23/p2.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 class Solution:
@@ -15,7 +17,25 @@ class Solution:
         longest_clique.sort()
         return ",".join(longest_clique)
 
+    def visualize(self, clique: set):
+        node_colors = [
+            "red" if n in clique else "steelblue" for n in self.graph.nodes()
+        ]
+        pos = nx.spring_layout(self.graph, seed=42)
+        nx.draw(
+            self.graph,
+            pos,
+            node_color=node_colors,
+            with_labels=True,
+            node_size=200,
+            font_size=7,
+        )
+        plt.title("Max clique nodes in red")
+        plt.show()
+
 
 if __name__ == "__main__":
-    sol = Solution("input")
-    print(sol.find_max_len_clique())
+    sol = Solution(Path(__file__).parent / "input")
+    clique_str = sol.find_max_len_clique()
+    print(clique_str)
+    sol.visualize(set(clique_str.split(",")))
