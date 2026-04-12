9ae9f861bb907eecd72d5bada2952610e196039d
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-12 14:22:27 +0300

    [Python] Add LCQ binary search tree P1-P3.

---
+++ LC_Python/quests/binarySearchTree/Insert.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return TreeNode(val)
+
+        curr = root
+        while True:
+            if curr.val >= val:
+                if curr.left is None:
+                    curr.left = TreeNode(val)
+                    break
+                else:
+                    curr = curr.left
+            else:
+                if curr.right is None:
+                    curr.right = TreeNode(val)
+                    break
+                else:
+                    curr = curr.right
+
+        return root
