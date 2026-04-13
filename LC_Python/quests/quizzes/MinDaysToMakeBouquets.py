1bab579122b7ff2a01e8342c2622b357107af67b
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-13 15:25:26 +0300

    [Python] Add prelim. LCQ sorting quiz 2 P1 - TLE.
    O(n*logn + m*n) -> TLE

---
+++ LC_Python/quests/quizzes/MinDaysToMakeBouquets.py
@@ -0,0 +1,52 @@
+from typing import List
+from heapq import heappush, heappop
+
+
+def createRollingMax(days: int, k: int) -> int:
+    n = len(days)
+
+    # rollingMax[idx] = maximum value from max(0, idx-k+1) to days[idx].
+    rollingMax = []
+
+    heap = []
+
+    for idx in range(n):
+        heappush(heap, (-days[idx], idx))
+        while heap[0][1] < max(0, idx - k + 1):
+            heappop(heap)
+        rollingMax.append(-heap[0][0])
+
+    return rollingMax
+
+
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        n = len(bloomDay)
+
+        if n < m * k:
+            return -1
+
+        prev = [0 for _ in range(n)]
+        rollingMax = createRollingMax(bloomDay, k)
+
+        for jj in range(1, m + 1):
+            curr = [float("inf") for _ in range(n)]
+            if jj == 1:
+                curr[k - 1] = rollingMax[k - 1]
+            for ii in range(k, n):
+                curr[ii] = min(
+                    curr[ii - 1],
+                    max(
+                        rollingMax[ii],
+                        prev[ii - k],
+                    ),
+                )
+            prev = curr
+
+        return min(prev)
+
+
+sol = Solution()
+print(sol.minDays([1, 10, 3, 10, 2], 3, 1))
+print(sol.minDays([1, 10, 3, 10, 2], 3, 2))
+print(sol.minDays([7, 7, 7, 7, 12, 7, 7], 2, 3))
