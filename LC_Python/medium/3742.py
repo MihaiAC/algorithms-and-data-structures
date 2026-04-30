3502a80e383fa47dc45af38a724f62a3ba8ae896
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-30 10:50:14 +0300

    [Python] Improve LC 3742 (9190ms, 21MB).
    Only need one dictionary.

--- LC_Python/medium/3742.py
+++ LC_Python/medium/3742.py
@@ -4,48 +4,35 @@ from typing import List
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         M, N = len(grid), len(grid[0])
-        prev_row = [{} for _ in range(N)]
-        curr_row = [{} for _ in range(N)]
-        curr_row[0][0] = 0
+        dp = [{} for _ in range(N)]
+        dp[0][0] = 0
 
         for ii in range(M):
-            for jj in range(1, N):
+            for jj in range(N):
+                prev_row = dp[jj].copy()
                 curr_cost = 0 if grid[ii][jj] == 0 else 1
                 for c in range(k + 1):
-                    if c < curr_cost and c in curr_row[jj]:
-                        del curr_row[jj][c]
+                    if c < curr_cost and c in dp[jj]:
+                        del dp[jj][c]
                         continue
 
                     prev_max = None
 
-                    if ii > 0 and c - curr_cost in prev_row[jj]:
-                        prev_max = prev_row[jj][c - curr_cost]
+                    if c - curr_cost in prev_row:
+                        prev_max = prev_row[c - curr_cost]
 
-                    if jj > 0 and c - curr_cost in curr_row[jj - 1]:
+                    if jj > 0 and c - curr_cost in dp[jj - 1]:
                         if prev_max is None:
-                            prev_max = curr_row[jj - 1][c - curr_cost]
+                            prev_max = dp[jj - 1][c - curr_cost]
                         else:
-                            prev_max = max(prev_max, curr_row[jj - 1][c - curr_cost])
+                            prev_max = max(prev_max, dp[jj - 1][c - curr_cost])
 
                     if prev_max is not None:
-                        curr_row[jj][c] = grid[ii][jj] + prev_max
-                    elif c in curr_row[jj]:
-                        del curr_row[jj][c]
+                        dp[jj][c] = grid[ii][jj] + prev_max
+                    elif c in dp[jj]:
+                        del dp[jj][c]
 
-            # Update curr_row[0][0..k].
-            if ii < M - 1:
-                prev_row, curr_row = curr_row, prev_row
-                curr_cost = 0 if grid[ii + 1][0] == 0 else 1
-                for c in range(0, k + 1):
-                    if c < curr_cost:
-                        if c in curr_row[0]:
-                            del curr_row[0][c]
-                    elif c - curr_cost in prev_row[0]:
-                        curr_row[0][c] = prev_row[0][c - curr_cost] + grid[ii + 1][0]
-                    elif c in curr_row[0]:
-                        del curr_row[0][c]
-
-        scores = curr_row[N - 1].values()
+        scores = dp[N - 1].values()
         return -1 if len(scores) == 0 else max(scores)
 
 
