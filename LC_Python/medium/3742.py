84776cd63f2c8d8d9c0e7ad74d8e764612e0d16a
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-30 10:09:34 +0300

    [Python] Improve LC 3742 (11784ms, 29MB).
    Need to figure out the naming convention for those commits.

--- LC_Python/medium/3742.py
+++ LC_Python/medium/3742.py
@@ -6,24 +6,38 @@ MINUS_INF = float("-inf")
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         M, N = len(grid), len(grid[0])
-        dp = [[[MINUS_INF] * (k + 1) for _ in range(N)] for _ in range(M)]
-        dp[0][0][0] = 0
+        prev_row = [[MINUS_INF] * (k + 1) for _ in range(N)]
+        curr_row = [[MINUS_INF] * (k + 1) for _ in range(N)]
+        curr_row[0][0] = 0
 
         for ii in range(M):
-            for jj in range(N):
+            for jj in range(1, N):
                 curr_cost = 0 if grid[ii][jj] == 0 else 1
-                for c in range(curr_cost, k + 1):
+                for c in range(k + 1):
+                    if c < curr_cost:
+                        curr_row[jj][c] = MINUS_INF
+                        continue
+
                     prev_max = MINUS_INF
 
                     if ii > 0:
-                        prev_max = max(prev_max, dp[ii - 1][jj][c - curr_cost])
+                        prev_max = max(prev_max, prev_row[jj][c - curr_cost])
                     if jj > 0:
-                        prev_max = max(prev_max, dp[ii][jj - 1][c - curr_cost])
+                        prev_max = max(prev_max, curr_row[jj - 1][c - curr_cost])
 
-                    if prev_max != MINUS_INF:
-                        dp[ii][jj][c] = grid[ii][jj] + prev_max
+                    curr_row[jj][c] = grid[ii][jj] + prev_max
 
-        ans = max(dp[M - 1][N - 1])
+            # Update curr_row[0][0..k].
+            if ii < M - 1:
+                prev_row, curr_row = curr_row, prev_row
+                curr_cost = 0 if grid[ii + 1][0] == 0 else 1
+                for c in range(0, k + 1):
+                    if c < curr_cost:
+                        curr_row[0][c] = MINUS_INF
+                    else:
+                        curr_row[0][c] = prev_row[0][c - curr_cost] + grid[ii + 1][0]
+
+        ans = max(curr_row[N - 1])
         return -1 if ans == MINUS_INF else ans
 
 
