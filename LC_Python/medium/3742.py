62d74ce94b0cc4ad8e9efdcbe57f44b2dd45ccae
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-30 11:31:29 +0300

    [Python] Iterate only over available costs - LC 3742 (3197ms, 21MB).

--- LC_Python/medium/3742.py
+++ LC_Python/medium/3742.py
@@ -5,6 +5,7 @@ class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         M, N = len(grid), len(grid[0])
         dp = [{} for _ in range(N)]
+        # dp[jj][c] = max score for current row, jj col, with c spent
         dp[0][0] = 0
 
         for ii in range(M):
@@ -12,20 +13,18 @@ class Solution:
                 prev_row = dp[jj].copy()
                 curr_cost = 0 if grid[ii][jj] == 0 else 1
                 curr_row = {}
-                for c in range(k + 1):
-                    prev_max = None
 
-                    if c - curr_cost in prev_row:
-                        prev_max = prev_row[c - curr_cost]
+                if jj > 0:
+                    for c, prev_max in dp[jj - 1].items():
+                        if c + curr_cost <= k:
+                            curr_row[c + curr_cost] = grid[ii][jj] + prev_max
 
-                    if jj > 0 and c - curr_cost in dp[jj - 1]:
-                        if prev_max is None:
-                            prev_max = dp[jj - 1][c - curr_cost]
-                        else:
-                            prev_max = max(prev_max, dp[jj - 1][c - curr_cost])
+                for c, prev_max in prev_row.items():
+                    if c + curr_cost <= k:
+                        curr_row[c + curr_cost] = max(
+                            grid[ii][jj] + prev_max, curr_row.get(c + curr_cost, -1)
+                        )
 
-                    if prev_max is not None:
-                        curr_row[c] = grid[ii][jj] + prev_max
                 dp[jj] = curr_row
 
         scores = dp[N - 1].values()
