c7602276edeb313d44dc17b73b1de710cb1c6b32
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-30 10:32:32 +0300

    [Python] Improve LC 3742 (9536ms, 21MB).

--- LC_Python/medium/3742.py
+++ LC_Python/medium/3742.py
@@ -1,31 +1,36 @@
 from typing import List
 
-MINUS_INF = float("-inf")
-
 
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         M, N = len(grid), len(grid[0])
-        prev_row = [[MINUS_INF] * (k + 1) for _ in range(N)]
-        curr_row = [[MINUS_INF] * (k + 1) for _ in range(N)]
+        prev_row = [{} for _ in range(N)]
+        curr_row = [{} for _ in range(N)]
         curr_row[0][0] = 0
 
         for ii in range(M):
             for jj in range(1, N):
                 curr_cost = 0 if grid[ii][jj] == 0 else 1
                 for c in range(k + 1):
-                    if c < curr_cost:
-                        curr_row[jj][c] = MINUS_INF
+                    if c < curr_cost and c in curr_row[jj]:
+                        del curr_row[jj][c]
                         continue
 
-                    prev_max = MINUS_INF
+                    prev_max = None
 
-                    if ii > 0:
-                        prev_max = max(prev_max, prev_row[jj][c - curr_cost])
-                    if jj > 0:
-                        prev_max = max(prev_max, curr_row[jj - 1][c - curr_cost])
+                    if ii > 0 and c - curr_cost in prev_row[jj]:
+                        prev_max = prev_row[jj][c - curr_cost]
 
-                    curr_row[jj][c] = grid[ii][jj] + prev_max
+                    if jj > 0 and c - curr_cost in curr_row[jj - 1]:
+                        if prev_max is None:
+                            prev_max = curr_row[jj - 1][c - curr_cost]
+                        else:
+                            prev_max = max(prev_max, curr_row[jj - 1][c - curr_cost])
+
+                    if prev_max is not None:
+                        curr_row[jj][c] = grid[ii][jj] + prev_max
+                    elif c in curr_row[jj]:
+                        del curr_row[jj][c]
 
             # Update curr_row[0][0..k].
             if ii < M - 1:
@@ -33,12 +38,15 @@ class Solution:
                 curr_cost = 0 if grid[ii + 1][0] == 0 else 1
                 for c in range(0, k + 1):
                     if c < curr_cost:
-                        curr_row[0][c] = MINUS_INF
-                    else:
+                        if c in curr_row[0]:
+                            del curr_row[0][c]
+                    elif c - curr_cost in prev_row[0]:
                         curr_row[0][c] = prev_row[0][c - curr_cost] + grid[ii + 1][0]
+                    elif c in curr_row[0]:
+                        del curr_row[0][c]
 
-        ans = max(curr_row[N - 1])
-        return -1 if ans == MINUS_INF else ans
+        scores = curr_row[N - 1].values()
+        return -1 if len(scores) == 0 else max(scores)
 
 
 sol = Solution()
