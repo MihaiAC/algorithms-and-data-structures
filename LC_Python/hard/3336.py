6b6954c3f37104ce953b366468a9d38b0bf74c68
Mihai Ciobanu <16577307+VoidUnderflow@users.noreply.github.com>
2026-07-14 19:49:49 +0100

    [Python] Improve LC 3336 (13167ms, 107MB).

--- LC_Python/hard/3336.py
+++ LC_Python/hard/3336.py
@@ -15,19 +15,22 @@ class Solution:
         for idx in range(N):
             for left_gcd in range(M + 1):
                 for right_gcd in range(M + 1):
+                    curr = dp[idx][left_gcd][right_gcd]
+
                     dp[idx + 1][left_gcd][right_gcd] = (
-                        dp[idx + 1][left_gcd][right_gcd] + dp[idx][left_gcd][right_gcd]
+                        dp[idx + 1][left_gcd][right_gcd] + curr
                     ) % MODN
 
                     _left_gcd = gcd(left_gcd, nums[idx])
                     dp[idx + 1][_left_gcd][right_gcd] = (
-                        dp[idx + 1][_left_gcd][right_gcd] + dp[idx][left_gcd][right_gcd]
+                        dp[idx + 1][_left_gcd][right_gcd] + curr
                     ) % MODN
 
                     _right_gcd = gcd(right_gcd, nums[idx])
                     dp[idx + 1][left_gcd][_right_gcd] = (
-                        dp[idx + 1][left_gcd][_right_gcd] + dp[idx][left_gcd][right_gcd]
+                        dp[idx + 1][left_gcd][_right_gcd] + curr
                     ) % MODN
+
         ans = 0
         for common_gcd in range(1, M + 1):
             ans = (ans + dp[N][common_gcd][common_gcd]) % MODN
