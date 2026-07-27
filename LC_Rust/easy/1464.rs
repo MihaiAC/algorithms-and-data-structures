50a02a89b516d05947ae6ccdeb16ccf1aa61ca35
Mihai Ciobanu <16577307+VoidUnderflow@users.noreply.github.com>
2026-07-27 17:56:17 +0100

    [Rust] Add daily - LC 1464 (0ms, 2.08MB).

---
+++ LC_Rust/easy/1464.rs
@@ -0,0 +1,26 @@
+struct Solution;
+
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut max1 = 0;
+        let mut max2 = 0;
+
+        for num in nums {
+            if num > max1 {
+                max2 = max1;
+                max1 = num;
+            } else if num > max2 {
+                max2 = num;
+            }
+        }
+
+        (max1 - 1) * (max2 - 1)
+    }
+}
+
+pub fn main() {
+    assert_eq!(Solution::max_product(vec![3, 4, 5, 2]), 12);
+    assert_eq!(Solution::max_product(vec![1, 5, 4, 5]), 16);
+    assert_eq!(Solution::max_product(vec![3, 7]), 12);
+    println!("ok");
+}
