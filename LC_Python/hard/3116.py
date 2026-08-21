08a05d4bf65ec11cf6725d411941bdf6ce43124e
Mihai Ciobanu <16577307+VoidUnderflow@users.noreply.github.com>
2026-08-21 21:01:05 +0100

    [Python] Add daily - LC 3116 (500ms, 19.51MB).

---
+++ LC_Python/hard/3116.py
@@ -0,0 +1,27 @@
+from typing import List
+from itertools import combinations
+from math import lcm
+from bisect import bisect_left
+
+
+class Solution:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        N = len(coins)
+
+        def count_gt_k(mid: int):
+            """
+            Returns true if the number of multiples <= mid
+            is greater than k.
+            """
+            count = 0
+            for take_k in range(1, N + 1):
+                for coin_combination in combinations(coins, take_k):
+                    count += mid // lcm(*coin_combination) * (-1) ** (take_k + 1)
+            return count >= k
+
+        return bisect_left(range(k * min(coins) + 1), True, lo=1, key=count_gt_k)
+
+
+sol = Solution()
+print(sol.findKthSmallest([3, 6, 9], 3))
+print(sol.findKthSmallest([5, 2], 7))
