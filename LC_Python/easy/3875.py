2fe49a47279645f8a405575e5b4045a19ca3abc1
Mihai Ciobanu <16577307+VoidUnderflow@users.noreply.github.com>
2026-09-02 12:39:42 +0100

    [Python] Add daily - LC 3875 (0ms, 19.23MB).

---
+++ LC_Python/easy/3875.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def uniformArray(self, nums: List[int]) -> bool:
+        return True
+
+
+sol = Solution()
+print(sol.uniformArray([2, 3]))
+print(sol.uniformArray([4, 6]))
